ee0b3df8b9041f9f394c3e8a22466218cc3353b3
Susumu Seino <seino@susu.mu>
2018-03-31 12:50:56 +0900

    Change path

--- wp-favorite-posts.php
+++ wp-favorite-posts.php
@@ -27,7 +27,7 @@ Author URI: https://github.com/hberberoglu
 
 */
 
-define('WPFP_PATH', plugins_url() . '/wp-favorite-posts');
+define('WPFP_PATH', plugins_url() . '/wp-favorite-posts-custom-fix');
 define('WPFP_META_KEY', "wpfp_favorites");
 define('WPFP_USER_OPTION_KEY', "wpfp_useroptions");
 define('WPFP_COOKIE_KEY', "wp-favorite-posts");
